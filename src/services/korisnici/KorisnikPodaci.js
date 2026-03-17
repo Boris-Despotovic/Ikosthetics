946fd54b05a44f17c866cbc0ef804196af6767bd
Boris-Despotovic <mrtecktonikboky@gmail.com>
2026-03-17 20:56:13 +0100

    mini update

--- src/services/korisnici/KorisnikPodaci.js
+++ src/services/korisnici/KorisnikPodaci.js
@@ -1,20 +1,16 @@
 export const korisnici=[
     {
         sifra: 1,
-        naziv korisnika: 'David',
-        naziv plana treninga: 'Muscle growth',
-        trajanje: 90,
-        cijena: 300,
-        datumPokretanja: '2025-01-21T17:00:00',
-        aktivan: false
+        ime: 'David',
+        prezime: 'Pokrajac'
+        NazivPlanaTreninga: 'Muscle growth',
+        trajanje: 90,    
     },
     {
         sifra: 2,
-        naziv korisnika: 'Fabijan',
-        naziv plana treninga: 'Weight loss',
-        trajanje: 90,
-        cijena: 400,
-        datumPokretanja: '2026-01-21T17:00:00',
-        aktivan: true
+        ime: 'Fabijan'
+        prezime: 'Turić'
+        NazivPlanaTreninga: 'Weight loss',
+        trajanje: 90,        
     }
 ]
