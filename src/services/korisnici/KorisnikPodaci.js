2d2115b5e6e1be7da90d8f6a30144802b47f2ec3
Boris-Despotovic <mrtecktonikboky@gmail.com>
2026-03-27 19:55:27 +0100

    dodavanje još korisnika

--- src/services/korisnici/KorisnikPodaci.js
+++ src/services/korisnici/KorisnikPodaci.js
@@ -12,5 +12,19 @@ export const korisnici=[
         prezime: 'Turić',
         nazivPlanaTreninga: 'Weight loss',
         trajanje: 90     
+    },
+    {
+        sifra: 3,
+        ime: 'Sebastijan',
+        prezime: 'Fariš',
+        nazivPlanaTreninga: 'Vertical jump increase',
+        trajanje: 90     
+    },
+    {
+        sifra: 4,
+        ime: 'Dario',
+        prezime: 'Despotović',
+        nazivPlanaTreninga: 'Condition',
+        trajanje: 90     
     }
 ]
